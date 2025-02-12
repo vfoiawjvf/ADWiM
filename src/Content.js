259f98926108edaba6d3e0166ee274ecc280f3aa
vfoiawjvf <124045190+vfoiawjvf@users.noreply.github.com>
2025-02-12 16:30:18 +0100

    Add files via upload

---
+++ src/Content.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+
+const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "amongus",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "amongus",
+    },
+    {
+      id: 3,
+      checked: true,
+      item: "amongus",
+    },
+    {
+      id: 4,
+      checked: false,
+      item: "amongus",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  };
+
+  return (
+    <main>
+    {items.length ? (
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked} />
+            <label onDoubleClick={() => handleCheck(item.id)} style={(item.checked) ? {textDecoration: 'line-through'} : null}>{item.item}</label>
+            <FaTrashAlt onClick={() => handleDelete(item.id)} role="button" tabIndex="0" />
+          </li>
+        ))}
+      </ul>
+      ) : (
+        <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
+      )};
+    </main>
+  )
+};
+
+export default Content;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header()
+{
+    const Header_style = { backgroundColor: 'mediumblue', color: '#fff'}
+    return(
+        <header style={Header_style}>
+            <h1>Groceries List</h1>
+        </header>
+    )   
+}
+
+export default Header
